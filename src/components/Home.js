01a7d74b5c1701aa9111ce7b7ed3dbd2542b9fbb
Leah Dombrovsky <leahliya59@gmail.com>
2017-10-18 09:01:01 -0500

    working version

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,32 @@ import Hero from './subcomponents/Hero';
 import BlogThumb from './subcomponents/BlogThumb';
 
 // import axios
+import axios from 'axios';
 
 class Home extends Component{
     constructor(){
         super();
         this.state = {
             index: 0,
-            posts: [{title: "Loading...",image: 'https://unsplash.it/900/400/?random'}]
+            posts: [{title: "Loading...",image: 'https://unsplash.it/900/400/?random'}],
+            featured: []
         }
     }
 
     // insert componentWillMount:
-    
+    componentDidMount(){
+        axios.get('/api/featured').then(result=>{
+            this.setState({
+                featured: result.data,
+                index: (~~(Math.random() * result.data.length) + 0),
+                posts: result.data
+            })
+        }).catch(console.log)
+    }
 
     render(){
         // map over your recommended blogs here, replace null.
-        const posts = null
+        const posts = this.state.posts.map((c,i)=><BlogThumb key={i} blog={c}/>);
 
         return(
             <div className="content" >
